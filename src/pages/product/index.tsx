8345a53bfc7493bf1703266572070dc6e242d4c6
hafiz-aa <mhafiz.aa@gmail.com>
2023-09-27 16:18:24 +0700

    Server-side rendering

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,12 +1,8 @@
+import { fetcher } from "@/lib/swr/fetcher";
+import ProductView from "@/views/product";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-
-type productType = {
-  id: number;
-  name: string;
-  price: number;
-  size: string;
-};
+import useSWR from "swr";
 
 const ProductPage = () => {
   const [isLogin, setIsLogin] = useState(true);
@@ -17,21 +13,21 @@ const ProductPage = () => {
     if (!isLogin) {
       push("/auth/login");
     }
-  }, []);
+  }, [isLogin, push]);
 
-  useEffect(() => {
-    fetch("/api/product")
-      .then((res) => res.json())
-      .then((response) => {
-        setProducts(response.data);
-      });
-  }, []);
+  const { data, error, isLoading } = useSWR("/api/product", fetcher);
+
+  // useEffect(() => {
+  //   fetch("/api/product")
+  //     .then((res) => res.json())
+  //     .then((response) => {
+  //       setProducts(response.data);
+  //     });
+  // }, []);
+
   return (
     <div>
-      <h1>Product Page</h1>
-      {products.map((product: productType) => (
-        <div key={product.id}>{product.name}</div>
-      ))}
+      <ProductView products={isLoading ? [] : data.data} />
     </div>
   );
 };
