b9f72b21e8c41d5035f50e519fc4b48edde752de
hafiz-aa <mhafiz.aa@gmail.com>
2023-10-04 13:53:58 +0700

    Middleware

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -5,15 +5,14 @@ import { useEffect, useState } from "react";
 import useSWR from "swr";
 
 const ProductPage = () => {
-  const [isLogin, setIsLogin] = useState(true);
   const [products, setProducts] = useState([]);
   const { push } = useRouter();
 
-  useEffect(() => {
-    if (!isLogin) {
-      push("/auth/login");
-    }
-  }, [isLogin, push]);
+  // useEffect(() => {
+  //   if (!isLogin) {
+  //     push("/auth/login");
+  //   }
+  // }, [isLogin, push]);
 
   // Client Side Rendering using SWR
   const { data, error, isLoading } = useSWR("/api/product", fetcher);
